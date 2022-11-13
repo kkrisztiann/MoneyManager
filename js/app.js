0eab5ed4c089fe2557347ee2225e356354e7099f
kkrisztiann <kerteszkrisztian@turr.hu>
2022-11-13 18:11:08 +0100

    nem jo meg

---
+++ js/app.js
@@ -0,0 +1,97 @@
+let app = new angular.module('Money', ['ngRoute']);
+
+app.run(function($rootScope){
+    $rootScope.title = 'Money Manager'  
+$rootScope.links = [
+    {
+        name:'Egyenleg',
+        url:'/egyenleg'
+    },
+    {
+        name:'+ / -',
+        url:'/pluszminusz'
+    },
+    {
+        name:'Kiadasok / bevételek',
+        url:'kiadbevesz'
+    },
+    {
+    name: 'Grafikon',
+    url: '/graf'
+    }
+    ];  
+});
+app.config(function($routeProvider) {
+    $routeProvider
+        .when('/egyenleg', {
+            templateUrl: 'egyenleg.html',
+            controller: 'egyenlegCtrl'
+    })
+        .when('/pluszminusz', {
+            templateUrl: 'pluszminusz.html',
+            controller: 'pluszminuszCtrl'
+    })
+        .when('/kiadbevesz', {
+            templateUrl: 'kiadbevesz.html',
+            controller: 'kiadbeveszCtrl'
+    })
+        .when('/graf', {
+            templateUrl: 'grafikon.html',
+            controller: 'grafCtrl'
+    })
+        .otherwise('index.html')
+});
+function Regisztracio(){
+
+    let regemail = document.querySelector('#regemail').value;
+    let regnev = document.querySelector('#regnev').value;
+    let regjelszo = document.querySelector('#regjelszo').value;
+    let regjelszo2 = document.querySelector('#regjelszo2').value;
+    
+    if(regnev != "" && regjelszo != "" && regemail != "" && regjelszo2 != ""){
+        if(regjelszo == regjelszo2)
+        {
+            const felhasznalo  = {
+                email: regemail,
+                name: regnev,
+                passwd: regjelszo,
+                bevetel: 0,
+                kiadas: 0
+            }
+            window.localStorage.setItem('user', JSON.stringify(felhasznalo));
+            console.log(felhasznalo);
+    
+            alert("Sikeres regisztráció")
+            window.open('/index.html','_self');
+            
+        }
+        else{
+            alert("Nem egyezik a két jelszó")
+        }
+    }
+    else{
+        alert("Nem adtál meg minden adatot");
+    }
+    }
+    
+    function Register(){
+        open('views/regisztracio.html', '_self');
+    }
+function Belepes(){   
+    
+let nev = document.querySelector('#nev').value;
+let jelszo = document.querySelector('#jelszo').value;
+
+let user = localStorage.getItem("user");
+
+
+console.log(user);
+if(user.includes(nev) && nev.length > 3){
+    alert("van ilyen nev");
+    } 
+else{
+    alert("Nem adtál meg minden adatot");
+}
+}
+
+
---
+++ js/controllers/pluszminuszCtrl.js
@@ -0,0 +1,3 @@
+app.controller('pluszminuszCtrl', function($scope) {
+
+});
